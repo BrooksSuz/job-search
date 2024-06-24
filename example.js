030789576944713715fcb4940913de585c5e49d9
Brooks Susor <139877554+BrooksHaasDoor@users.noreply.github.com>
2024-06-24 16:03:47 -0400

    Add puppeteer

--- example.js
+++ example.js
@@ -1,16 +1,38 @@
-import * as cheerio from 'cheerio';
-import axios from 'axios';
+import puppeteer from 'puppeteer';
 
-const url = 'https://www.utoledo.edu/policies';
+const url = 'https://careers.utoledo.edu/cw/en-us/listing/';
 
-axios
-	.get(url)
-	.then((res) => {
-		const html = res.data;
-		const $ = cheerio.load(html);
-		const firstH1 = $('h1').first().html();
-		console.log(firstH1);
-	})
-	.catch((err) => {
-		console.error('Error fetching the webpage', err);
-	});
+const goFunction = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto(url);
+  await page.setViewport({ width: 1080, height: 1024 });
+
+  // Recursively click "More Jobs" button until it is no longer on the page
+  const clickButtonRecursively = async (selector) => {
+    try {
+      await page.evaluate((selector) => {
+        const element = document.querySelector(selector);
+        if (element) {
+          element.scrollIntoView();
+        }
+      }, selector);
+      await page.waitForSelector(selector);
+      await page.click(selector);
+
+      console.log('Button clicked');
+      await page.waitForTimeout(1000);
+      await clickButtonRecursively(selector);
+    } catch (err) {
+      console.log('Button not found, stopping recursion');
+    }
+  };
+
+  const anchorMoreJobs = '.more-link';
+
+  await clickButtonRecursively(anchorMoreJobs);
+  await browser.close();
+};
+
+goFunction();
