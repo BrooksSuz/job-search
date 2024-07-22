f63a31155a8025aac0a54e91c79d124f6f5ef97d
Brooks Susor <139877554+BrooksHaasDoor@users.noreply.github.com>
2024-07-22 08:57:55 -0400

    Learning the basics

--- example.js
+++ example.js
@@ -1,14 +1,26 @@
 import puppeteer from 'puppeteer';
 
-const url = 'https://example.com/';
+const url = 'https://careers.emich.edu/jobs/search';
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   await page.goto(url);
 
-  const href1 = 'https://www.iana.org/domains/example';
+  const btnConsentId = 'button#consent_agree';
+  const btnConsent = await page.$(btnConsentId);
+
+  try {
+    await page.waitForSelector(btnConsentId, { timeout: 5000 });
+    await page.click(btnConsentId);
+    console.log('Button clicked.');
+  } catch (err) {
+    console.log('Button not found or timed out.');
+  }
+
+  const href1 =
+    'https://careers.emich.edu/jobs/plumber-maintenance-ypsilanti-michigan-united-states-e830fd00-51f9-4cc1-b179-be4ab262c50b';
   const anchorTextContent1 = await page.evaluate((href) => {
     return document.querySelector(`a[href='${href}']`).textContent;
   }, href1);
