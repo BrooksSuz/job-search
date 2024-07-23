2dc7b786059274d925d0a8c8f5e98110daa3390b
Brooks Susor <139877554+BrooksHaasDoor@users.noreply.github.com>
2024-07-23 16:29:14 -0400

    Learning the basics

--- example.js
+++ example.js
@@ -1,10 +1,8 @@
 import puppeteer from 'puppeteer';
 
 const url = 'https://careers.emich.edu/jobs/search';
-const url = 'https://careers.emich.edu/jobs/search';
 
 (async () => {
-  const browser = await puppeteer.launch();
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
@@ -15,19 +13,10 @@ const url = 'https://careers.emich.edu/jobs/search';
   try {
     await page.waitForSelector(btnConsentId, { timeout: 5000 });
     await page.click(btnConsentId);
-    console.log('Button clicked.');
+    console.log('Consent button clicked.');
   } catch (err) {
-    console.log('Button not found or timed out.');
+    console.log('Consent button not found or timed out.');
   }
-
-  const href1 =
-    'https://careers.emich.edu/jobs/plumber-maintenance-ypsilanti-michigan-united-states-e830fd00-51f9-4cc1-b179-be4ab262c50b';
-
-  const anchorTextContent1 = await page.evaluate((href) => {
-    return document.querySelector(`a[href='${href}']`).textContent;
-  }, href1);
-
-  console.log(anchorTextContent1);
 
   const jobTitleId = 'link_job_title';
 
@@ -43,21 +32,6 @@ const url = 'https://careers.emich.edu/jobs/search';
   }, jobTitleId);
 
   console.log(jobListings);
-
-  const anchorClick1 = await page.evaluate((href1) => {
-    const anchor = document.querySelector(`a[href='${href1}']`);
-
-    if (anchor) {
-      anchor.click();
-      return 'First anchor clicked.';
-    }
-
-    return '';
-  }, href1);
-
-  if (!anchorClick1) await browser.close();
-
-  console.log(anchorClick1);
 
   await browser.close();
 })();
