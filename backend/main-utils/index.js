3e95a6fd3e0f6bd4565a7aef30b199c4e7e46893
Brooks Susor <139877554+BrooksHaasDoor@users.noreply.github.com>
2024-12-04 16:30:28 -0500

    Add index.js

---
+++ backend/main-utils/index.js
@@ -0,0 +1,4 @@
+// Simplify import
+export { default as extractListings } from './browser.js';
+export { default as createHtmlListings } from './html.js';
+export { default as updateDatabase } from './database.js';
