35287b2363280f9014cc8348fdffe0010c24376e
Brooks Susor <susorbrooks@gmail.com>
2024-12-12 20:54:02 -0500

    Fix logout

--- backend/passport-config.js
+++ backend/passport-config.js
@@ -4,31 +4,36 @@ import { Strategy as LocalStrategy } from 'passport-local';
 import User from './User.js';
 
 passport.use(
-  new LocalStrategy(
-    {
-      usernameField: 'email',
-      passwordField: 'password',
-    },
-    async (email, password, done) => {
-      try {
-        const user = await User.findOne({ email });
-        if (!user) return done(null, false, { message: 'User not found' });
+	new LocalStrategy(
+		{
+			usernameField: 'email',
+			passwordField: 'password',
+		},
+		async (email, password, done) => {
+			try {
+				const user = await User.findOne({ email });
+				if (!user) return done(null, false, { message: 'User not found' });
 
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch)
-          return done(null, false, { message: 'Incorrect password' });
+				const isMatch = await bcrypt.compare(password, user.password);
+				if (!isMatch)
+					return done(null, false, { message: 'Incorrect password' });
 
-        return done(null, user);
-      } catch (err) {
-        return done(err);
-      }
-    }
-  )
+				return done(null, user);
+			} catch (err) {
+				return done(err);
+			}
+		}
+	)
 );
 
 passport.serializeUser((user, done) => done(null, user.id));
-passport.deserializeUser((id, done) =>
-  User.findById(id, (err, user) => done(err, user))
-);
+passport.deserializeUser(async (id, done) => {
+	try {
+		const user = await User.findById(id).exec();
+		done(null, user);
+	} catch (err) {
+		done(err);
+	}
+});
 
 export default passport;
--- public/app.js
+++ public/app.js
@@ -9,139 +9,179 @@ btnGetListings.addEventListener('click', onGetListingsClick);
 const btnLogin = document.querySelector('.login');
 btnLogin.addEventListener('click', onLoginClick);
 
+// Log out on click
+const btnLogout = document.querySelector('.logout');
+btnLogout.addEventListener('click', onLogoutClick);
+
 // Register on click
 const btnRegister = document.querySelector('.register');
 btnRegister.addEventListener('click', onRegisterClick);
 
 async function onLoginClick() {
-  handleAccountClick('login');
+	const btnLogin = document.querySelector('.login');
+	const btnLogout = document.querySelector('.logout');
+	try {
+		await handleAccountClick('login');
+		await swapButtons(btnLogin, btnLogout);
+
+		const inputEmail = document.querySelector('.email');
+		const inputPassword = document.querySelector('.password');
+		inputEmail.disabled = true;
+		inputPassword.disabled = true;
+	} catch (err) {
+		console.error('Error in function onLoginClick', err);
+	}
+}
+
+async function onLogoutClick() {
+	const btnLogin = document.querySelector('.login');
+	const btnLogout = document.querySelector('.logout');
+	try {
+		await handleLogOutClick();
+		await swapButtons(btnLogout, btnLogin);
+	} catch (err) {
+		console.error('Error in function onLogoutClick', err);
+	}
+
+	const inputEmail = document.querySelector('.email');
+	const inputPassword = document.querySelector('.password');
+	inputEmail.disabled = false;
+	inputEmail.value = '';
+	inputPassword.disabled = false;
+	inputPassword.value = '';
 }
 
 async function onRegisterClick() {
-  handleAccountClick('register');
+	handleAccountClick('register');
 }
 
 async function onGetListingsClick() {
-  // Remove children from previous search
-  const divListings = document.querySelector('.listings');
-  if (divListings.firstChild) divListings.replaceChildren();
+	// Remove children from previous search
+	const divListings = document.querySelector('.listings');
+	if (divListings.firstChild) divListings.replaceChildren();
 
-  // Disable search elements
-  const btnGetListings = document.querySelector('.get-listings');
-  const inputsAdvanced = document.querySelectorAll(
-    '.container-advanced > label input'
-  );
-  btnGetListings.disabled = true;
-  inputsAdvanced.forEach((input) => {
-    input.disabled = true;
-  });
+	// Disable search elements
+	const btnGetListings = document.querySelector('.get-listings');
+	const inputsAdvanced = document.querySelectorAll(
+		'.container-advanced > label input'
+	);
+	btnGetListings.disabled = true;
+	inputsAdvanced.forEach((input) => {
+		input.disabled = true;
+	});
 
-  // Replace button text with spinner animation
-  const spanBtnListingsText = document.querySelector('.get-listings-text');
-  spanBtnListingsText.style.display = 'none';
-  const spanSpinner = document.querySelector('.spinner');
-  const stopSpinner = startSpinner(spanSpinner);
+	// Replace button text with spinner animation
+	const spanBtnListingsText = document.querySelector('.get-listings-text');
+	spanBtnListingsText.style.display = 'none';
+	const spanSpinner = document.querySelector('.spinner');
+	const stopSpinner = startSpinner(spanSpinner);
 
-  // Get configs from database
-  const arrConfigs = await fetchSiteConfigs();
+	// Get configs from database
+	const arrConfigs = await fetchSiteConfigs();
 
-  // Alphabetize and consume API endpoint
-  const arrAlphabetizedConfigs = alphabetizeConfigs(arrConfigs);
-  await executeJobSearch(arrAlphabetizedConfigs).finally(() => {
-    // Remove/stop the spinner and display original text
-    spanSpinner.classList.remove('show');
-    stopSpinner();
-    spanBtnListingsText.style.display = 'inline';
+	// Alphabetize and consume API endpoint
+	const arrAlphabetizedConfigs = alphabetizeConfigs(arrConfigs);
+	await executeJobSearch(arrAlphabetizedConfigs).finally(() => {
+		// Remove/stop the spinner and display original text
+		spanSpinner.classList.remove('show');
+		stopSpinner();
+		spanBtnListingsText.style.display = 'inline';
 
-    // Re-enable search elements
-    btnGetListings.disabled = false;
-    inputsAdvanced.forEach((input) => {
-      input.disabled = false;
-    });
+		// Re-enable search elements
+		btnGetListings.disabled = false;
+		inputsAdvanced.forEach((input) => {
+			input.disabled = false;
+		});
 
-    // Send mail
-    // TODO: Figure out what to do with this (it works)
-    // sendListingsHTML();
-  });
+		// Send mail
+		// TODO: Figure out what to do with this (it works)
+		// sendListingsHTML();
+	});
 }
 
 const handleAccountClick = async (strRoute) => {
-  const inputEmail = document.querySelector('.email');
-  const inputPassword = document.querySelector('.password');
-  const email = inputEmail.value.trim();
-  const password = inputPassword.value.trim();
+	const inputEmail = document.querySelector('.email');
+	const inputPassword = document.querySelector('.password');
+	const email = inputEmail.value.trim();
+	const password = inputPassword.value.trim();
 
-  if (!email || !password) {
-    console.error('Email and password cannot be empty.');
-    return;
-  }
+	if (!email || !password) {
+		console.error('Email and password cannot be empty.');
+		return;
+	}
 
-  try {
-    const response = await fetch(`/auth/${strRoute}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-    });
+	try {
+		const response = await fetch(`/auth/${strRoute}`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				email,
+				password,
+			}),
+		});
 
-    if (response.ok) {
-      inputEmail.value = '';
-      inputPassword.value = '';
-    } else {
-      const responseText = await response.text();
-      console.log(responseText);
-    }
-  } catch (err) {
-    console.error(`Error in function handleAccountClick`, err);
-  }
+		if (!response.ok) {
+			const responseText = await response.text();
+			console.log(responseText);
+		}
+	} catch (err) {
+		console.error(`Error in function handleAccountClick`, err);
+	}
+};
+
+const swapButtons = async (btnNone, btnBlock) => {
+	btnNone.style.display = 'none';
+	btnBlock.style.display = 'block';
+};
+
+const handleLogOutClick = async () => {
+	await fetch('/auth/logout');
 };
 
 const startSpinner = (spanSpinner) => {
-  const spinnerChars = ['|', '/', '-', '\\'];
-  spanSpinner.classList.add('show');
+	const spinnerChars = ['|', '/', '-', '\\'];
+	spanSpinner.classList.add('show');
 
-  let i = 0;
-  const spinnerInterval = setInterval(() => {
-    spanSpinner.textContent = spinnerChars[i];
-    i = (i + 1) % spinnerChars.length;
-  }, 90);
+	let i = 0;
+	const spinnerInterval = setInterval(() => {
+		spanSpinner.textContent = spinnerChars[i];
+		i = (i + 1) % spinnerChars.length;
+	}, 90);
 
-  return () => clearInterval(spinnerInterval);
+	return () => clearInterval(spinnerInterval);
 };
 
 const fetchSiteConfigs = async () => {
-  try {
-    const response = await fetch('/api/site-configs');
-    const arrConfigs = await response.json();
-    return arrConfigs;
-  } catch (err) {
-    console.error('Error fetching site configs:', err);
-  }
+	try {
+		const response = await fetch('/api/site-configs');
+		const arrConfigs = await response.json();
+		return arrConfigs;
+	} catch (err) {
+		console.error('Error fetching site configs:', err);
+	}
 };
 
 const alphabetizeConfigs = (arrConfigs) =>
-  [...arrConfigs].sort((a, b) =>
-    a.siteName < b.siteName ? -1 : a.siteName > b.siteName ? 1 : 0
-  );
+	[...arrConfigs].sort((a, b) =>
+		a.siteName < b.siteName ? -1 : a.siteName > b.siteName ? 1 : 0
+	);
 
 const sendListingsHTML = async () => {
-  const divListings = document.querySelector('.listings');
-  const divToString = divListings.outerHTML;
-  try {
-    const response = await fetch(`/api/send-mail`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ html: divToString }),
-    });
+	const divListings = document.querySelector('.listings');
+	const divToString = divListings.outerHTML;
+	try {
+		const response = await fetch(`/api/send-mail`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ html: divToString }),
+		});
 
-    if (response.ok) console.log('HTML sent successfully');
-  } catch (err) {
-    console.error('Error in function sendListingHTML', err);
-  }
+		if (response.ok) console.log('HTML sent successfully');
+	} catch (err) {
+		console.error('Error in function sendListingHTML', err);
+	}
 };
