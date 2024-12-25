71c70ba900d833f60ddb6c529fde27038e2a664e
Brooks <susorbrooks@gmail.com>
2024-12-25 14:19:33 -0500

    Up

--- backend/scrape-listings/find-listings.js
+++ backend/scrape-listings/find-listings.js
@@ -3,86 +3,86 @@ import navigateSite from './navigate-site.js';
 import logger from '../logger-backend.js';
 
 async function findListings(
-  page,
-  arrSearchTerms,
-  objConfigPairs,
-  incrementCount,
-  getCount,
-  arrAllScrapedListings = [] // Default array for recursion
+	page,
+	arrSearchTerms,
+	objConfigPairs,
+	incrementCount,
+	getCount,
+	arrAllScrapedListings = [] // Default array for recursion
 ) {
-  // Destructure configPairs and disperse
-  const {
-    consent: strConsent,
-    errorMessages: strErrorMessages,
-    isAnchor: strIsAnchor,
-    listing: strListing,
-    nextPageDisabled: strNextPageDisabled,
-    nextPageLink: strNextPageLink,
-    nextPageParent: strNextPageParent,
-  } = objConfigPairs;
+	// Destructure configPairs and disperse
+	const {
+		consent: strConsent,
+		errorMessages: strErrorMessages,
+		isAnchor: strIsAnchor,
+		listing: strListing,
+		nextPageDisabled: strNextPageDisabled,
+		nextPageLink: strNextPageLink,
+		nextPageParent: strNextPageParent,
+	} = objConfigPairs;
 
-  // Change errorMessages type
-  const arrErrorMessages = strErrorMessages.split(', ');
+	// Change errorMessages type
+	const arrErrorMessages = strErrorMessages.split(', ');
 
-  // Check consent only on the first call
-  const isFirstPage = !getCount();
-  if (strConsent && isFirstPage) await checkConsent(page, strConsent);
+	// Check consent only on the first call
+	const isFirstPage = !getCount();
+	if (strConsent && isFirstPage) await checkConsent(page, strConsent);
 
-  // Scrape listings on the current page
-  const arrFilteredListings = await filterListings(
-    page,
-    arrSearchTerms,
-    strListing
-  );
-  arrAllScrapedListings.push(...arrFilteredListings);
+	// Scrape listings on the current page
+	const arrFilteredListings = await filterListings(
+		page,
+		arrSearchTerms,
+		strListing
+	);
+	arrAllScrapedListings.push(...arrFilteredListings);
 
-  // Attempt to navigate to the next page
-  const boolHasNextPage = await navigateSite(
-    page,
-    arrErrorMessages,
-    strIsAnchor,
-    strNextPageDisabled,
-    strNextPageLink,
-    strNextPageParent
-  );
+	// Attempt to navigate to the next page
+	const boolHasNextPage = await navigateSite(
+		page,
+		arrErrorMessages,
+		strIsAnchor,
+		strNextPageDisabled,
+		strNextPageLink,
+		strNextPageParent
+	);
 
-  // Increment page count
-  incrementCount();
+	// Increment page count
+	incrementCount();
 
-  // Base case: No next page
-  if (!boolHasNextPage)
-    return alphabetizeScrapedListings([...arrAllScrapedListings]);
+	// Base case: No next page
+	if (!boolHasNextPage)
+		return alphabetizeScrapedListings([...arrAllScrapedListings]);
 
-  // Recursive case: Scrape the next page
-  return findListings(
-    page,
-    arrSearchTerms,
-    objConfigPairs,
-    incrementCount,
-    getCount,
-    arrAllScrapedListings
-  );
+	// Recursive case: Scrape the next page
+	return findListings(
+		page,
+		arrSearchTerms,
+		objConfigPairs,
+		incrementCount,
+		getCount,
+		arrAllScrapedListings
+	);
 }
 
 const checkConsent = async (page, strConsent) => {
-  try {
-    await page.waitForSelector(strConsent, { timeout: 5000 });
-    logger.info('Consent popup found.');
-    await page.click(strConsent);
-  } catch (err) {
-    if (err.name === 'TimeoutError') {
-      logger.info('Consent popup not found.');
-    } else {
-      logger.error('Error in function checkConsent:', err);
-    }
-  }
+	try {
+		await page.waitForSelector(strConsent, { timeout: 5000 });
+		logger.info('\nConsent popup found');
+		await page.click(strConsent);
+	} catch (err) {
+		if (err.name === 'TimeoutError') {
+			logger.info('\nConsent popup not found');
+		} else {
+			logger.error('Error in function checkConsent:', err);
+		}
+	}
 };
 
 const alphabetizeScrapedListings = (arrAllScrapedListings) =>
-  arrAllScrapedListings.sort((strTitle, strUrl) => {
-    const [strTitleKey] = Object.keys(strTitle);
-    const [strUrlKey] = Object.keys(strUrl);
-    return strTitleKey < strUrlKey ? -1 : strTitleKey > strUrlKey ? 1 : 0;
-  });
+	arrAllScrapedListings.sort((strTitle, strUrl) => {
+		const [strTitleKey] = Object.keys(strTitle);
+		const [strUrlKey] = Object.keys(strUrl);
+		return strTitleKey < strUrlKey ? -1 : strTitleKey > strUrlKey ? 1 : 0;
+	});
 
 export default findListings;
