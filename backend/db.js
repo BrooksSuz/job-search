b721d836c6e762ca7eeaa1ad9eee23c0c433d2e8
Brooks <susorbrooks@gmail.com>
2024-12-27 22:23:49 -0500

    Update fixie

--- backend/db.js
+++ backend/db.js
@@ -3,19 +3,15 @@ import mongoose from 'mongoose';
 import Site from './schemas/Site.js';
 import User from './schemas/User.js';
 import logger from './logger-backend.js';
-import { SocksProxyAgent } from 'socks-proxy-agent';
 dotenv.config();
 
 const uri = process.env.MONGO_URI;
 const db = process.env.DB;
-const fixieUrl = process.env.FIXIE_URL;
-const socksAgent = new SocksProxyAgent(fixieUrl);
 const environment = process.env.NODE_ENV;
 
 async function connectToDb() {
 	try {
 		const options = {
-			agent: environment === 'production' ? socksAgent : null,
 			dbName: db,
 		};
 
