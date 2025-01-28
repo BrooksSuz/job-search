ea4b2bf6419546a596e64cb90156931631bb4eb6
Brooks Susor <139877554+BrooksHaasDoor@users.noreply.github.com>
2025-01-28 17:01:30 -0500

    Remove conn

--- backend/queue.js
+++ backend/queue.js
@@ -9,7 +9,6 @@ const myQueue = new Queue('myQueue', process.env.REDIS_URL, {
 		port: process.env.REDIS_PORT,
 		host: process.env.REDIS_HOST,
     password: process.env.REDIS_PASSWORD,
-    conn
 	}
 });
 
