62136cc3127bcfe3e254e4c4e9a1feaf38d03777
Brooks Susor <139877554+BrooksHaasDoor@users.noreply.github.com>
2025-02-06 09:55:45 -0500

    Add worker.js

--- backend/queue.js
+++ backend/queue.js
@@ -1,85 +1,15 @@
 import Queue from 'bull';
 import dotenv from 'dotenv';
-import scrapeListings from './scrape-listings/scrape-listings.js';
-import { clients } from './server.js';
-import logger from './logger-backend.js';
 
 dotenv.config();
 
 const redisUrl = `redis://${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
+const queueName =
+	process.env.NODE_ENV === 'production' ? 'prodUserQueue' : 'devUserQueue';
+const userQueue = new Queue(queueName, redisUrl, {
+	defaultJobOptions: {
+		timeout: 300000,
+	},
+});
 
-let lastQueueCreationTime = 0;
-let userQueue = null;
-const createUserQueue = async () => {
-	const currentTime = Date.now();
-
-	if (!userQueue || currentTime - lastQueueCreationTime > 5000) {
-		const uuid = await fetch('https://www.uuidtools.com/api/generate/v4').then(
-			(res) => res.json()
-		);
-		logger.info(uuid);
-		const queueName =
-			process.env.NODE_ENV === 'production' ? `prod-${uuid}` : `dev-${uuid}`;
-
-		userQueue = new Queue(queueName, redisUrl, {
-			defaultJobOptions: {
-				timeout: 300000,
-			},
-		});
-	}
-
-	lastQueueCreationTime = currentTime;
-
-	userQueue.process(10, async (job) => {
-		const { keywords, objConfig } = job.data;
-		const jobId = job.id;
-		const interval = setInterval(() => {
-			clients.forEach((client) => {
-				if (client.readyState === 1) {
-					client.send(JSON.stringify({ jobId, status: 'in progress' }));
-				}
-			});
-		}, 5000);
-
-		try {
-			const listings = await scrapeListings(keywords, objConfig);
-			clearInterval(interval);
-			return listings;
-		} catch (err) {
-			clearInterval(interval);
-			logger.error(`Error processing job ${job.id}:\n${err}`);
-			throw new Error('Job processing failed');
-		}
-	});
-
-	userQueue.on('completed', async (job, result) => {
-		clients.forEach((client) => {
-			if (client.readyState === 1) {
-				client.send(
-					JSON.stringify({ jobId: job.id, status: 'completed', result })
-				);
-			}
-		});
-
-		const activeCount = await userQueue.getActiveCount();
-		if (!activeCount) await userQueue.obliterate();
-	});
-
-	userQueue.on('failed', (job, err) => {
-		clients.forEach((client) => {
-			if (client.readyState === 1) {
-				client.send(
-					JSON.stringify({
-						jobId: job.id,
-						status: 'failed',
-						error: err.message,
-					})
-				);
-			}
-		});
-	});
-
-	return userQueue;
-};
-
-export default createUserQueue;
+export default userQueue;
---
+++ backend/worker.js
@@ -0,0 +1,52 @@
+import userQueue from './queue.js';
+import scrapeListings from './scrape-listings/scrape-listings.js';
+import { clients } from './server.js';
+import logger from './logger-backend.js';
+
+userQueue.process(20, async (job) => {
+  const { keywords, objConfig } = job.data;
+  const jobId = job.id;
+  const interval = setInterval(() => {
+    clients.forEach((client) => {
+      if (client.readyState === 1) {
+        client.send(JSON.stringify({ jobId, status: 'in progress' }));
+      }
+    });
+  }, 5000);
+
+  try {
+    const listings = await scrapeListings(keywords, objConfig);
+    clearInterval(interval);
+    return listings;
+  } catch (err) {
+    clearInterval(interval);
+    logger.error(`Error processing job ${job.id}:\n${err}`);
+    throw new Error('Job processing failed');
+  }
+});
+
+userQueue.on('completed', async (job, result) => {
+  clients.forEach((client) => {
+    if (client.readyState === 1) {
+      client.send(
+        JSON.stringify({ jobId: job.id, status: 'completed', result })
+      );
+    }
+  });
+});
+
+userQueue.on('failed', (job, err) => {
+  clients.forEach((client) => {
+    if (client.readyState === 1) {
+      client.send(
+        JSON.stringify({
+          jobId: job.id,
+          status: 'failed',
+          error: err.message,
+        })
+      );
+    }
+  });
+});
+
+export default userQueue;
