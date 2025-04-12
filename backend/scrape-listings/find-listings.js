5d38692238dc2cbc576c0e78ddbcb28ba13e614a
BrooksSuz <susorbrooks@gmail.com>
2025-04-12 12:59:06 -0400

    Update import

--- backend/scrape-listings/find-listings.js
+++ backend/scrape-listings/find-listings.js
@@ -1,82 +1,82 @@
-import filterListings from "./filter-listings.js";
-import navigateSite from "./navigate-site.js";
-import logger from "../logger-backend.js";
+import filterListings from './filter-listings.js';
+import navigateSite from './navigate-site.js';
+import { logger } from '../logger-backend.js';
 
 async function findListings(
-  page,
-  arrSearchTerms,
-  objConfigPairs,
-  incrementCount,
-  getCount,
+	page,
+	arrSearchTerms,
+	objConfigPairs,
+	incrementCount,
+	getCount
 ) {
-  // Destructure configPairs and disperse
-  const {
-    consent: strConsent,
-    errorMessages: strErrorMessages,
-    isAnchor: boolIsAnchor,
-    listing: strListing,
-    nextPageDisabled: strNextPageDisabled,
-    nextPageLink: strNextPageLink,
-    nextPageParent: strNextPageParent,
-  } = objConfigPairs;
+	// Destructure configPairs and disperse
+	const {
+		consent: strConsent,
+		errorMessages: strErrorMessages,
+		isAnchor: boolIsAnchor,
+		listing: strListing,
+		nextPageDisabled: strNextPageDisabled,
+		nextPageLink: strNextPageLink,
+		nextPageParent: strNextPageParent,
+	} = objConfigPairs;
 
-  // Change errorMessages type
-  const arrErrorMessages = strErrorMessages.split(", ");
+	// Change errorMessages type
+	const arrErrorMessages = strErrorMessages.split(', ');
 
-  // Check consent only on the first call
-  const isFirstPage = !getCount();
-  if (strConsent && isFirstPage) await checkConsent(page, strConsent);
+	// Check consent only on the first call
+	const isFirstPage = !getCount();
+	if (strConsent && isFirstPage) await checkConsent(page, strConsent);
 
-  const arrAllScrapedListings = [];
+	const arrAllScrapedListings = [];
 
-  // Use a loop to navigate through pages
-  let hasNextPage = true;
+	// Use a loop to navigate through pages
+	let hasNextPage = true;
 
-  while (hasNextPage) {
-    // Scrape listings on the current page
-    const arrFilteredListings = await filterListings(
-      page,
-      arrSearchTerms,
-      strListing,
-    );
-    arrAllScrapedListings.push(...arrFilteredListings);
+	while (hasNextPage) {
+		// Scrape listings on the current page
+		const arrFilteredListings = await filterListings(
+			page,
+			arrSearchTerms,
+			strListing
+		);
+		arrAllScrapedListings.push(...arrFilteredListings);
 
-    // Attempt to navigate to the next page
-    hasNextPage = await navigateSite(
-      page,
-      arrErrorMessages,
-      boolIsAnchor,
-      strNextPageDisabled,
-      strNextPageLink,
-      strNextPageParent,
-    );
+		// Attempt to navigate to the next page
+		hasNextPage = await navigateSite(
+			page,
+			arrErrorMessages,
+			boolIsAnchor,
+			strNextPageDisabled,
+			strNextPageLink,
+			strNextPageParent
+		);
 
-    // Increment page count
-    incrementCount();
-  }
+		// Increment page count
+		incrementCount();
+	}
 
-  return alphabetizeScrapedListings(arrAllScrapedListings);
+	return alphabetizeScrapedListings(arrAllScrapedListings);
 }
 
 const checkConsent = async (page, strConsent) => {
-  try {
-    await page.waitForSelector(strConsent, { timeout: 5000 });
-    logger.info("\nConsent popup found.");
-    await page.click(strConsent);
-  } catch (err) {
-    if (err.name === "TimeoutError") {
-      logger.info("\nConsent popup not found.");
-    } else {
-      logger.error(`Error in function checkConsent:\n${err}`);
-    }
-  }
+	try {
+		await page.waitForSelector(strConsent, { timeout: 5000 });
+		logger.info('\nConsent popup found.');
+		await page.click(strConsent);
+	} catch (err) {
+		if (err.name === 'TimeoutError') {
+			logger.info('\nConsent popup not found.');
+		} else {
+			logger.error(`Error in function checkConsent:\n${err}`);
+		}
+	}
 };
 
 const alphabetizeScrapedListings = (arrAllScrapedListings) =>
-  arrAllScrapedListings.sort((strTitle, strUrl) => {
-    const [strTitleKey] = Object.keys(strTitle);
-    const [strUrlKey] = Object.keys(strUrl);
-    return strTitleKey < strUrlKey ? -1 : strTitleKey > strUrlKey ? 1 : 0;
-  });
+	arrAllScrapedListings.sort((strTitle, strUrl) => {
+		const [strTitleKey] = Object.keys(strTitle);
+		const [strUrlKey] = Object.keys(strUrl);
+		return strTitleKey < strUrlKey ? -1 : strTitleKey > strUrlKey ? 1 : 0;
+	});
 
 export default findListings;
--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,27 @@
-import MongoStore from "connect-mongo";
-import dotenv from "dotenv";
-import express from "express";
-import session from "express-session";
-import path from "path";
-import { fileURLToPath } from "url";
-import sendMail from "./scrape-listings/send-mail.js";
-import authRoutes from "./auth-backend.js";
+import MongoStore from 'connect-mongo';
+import dotenv from 'dotenv';
+import express from 'express';
+import session from 'express-session';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import sendMail from './scrape-listings/send-mail.js';
+import authRoutes from './auth-backend.js';
 import {
-  connectToDb,
-  deleteUser,
-  getPremadeConfigs,
-  getSelectedConfigs,
-  mongoose,
-} from "./db.js";
-import passport from "./passport-config.js";
-import Site from "./schemas/Site.js";
-import User from "./schemas/User.js";
-import logger from "./logger-backend.js";
-import cors from "cors";
-import Queue from "bull";
-import { WebSocketServer } from "ws";
-import http from "http";
-import Redis from "ioredis";
+	connectToDb,
+	deleteUser,
+	getPremadeConfigs,
+	getSelectedConfigs,
+	mongoose,
+} from './db.js';
+import passport from './passport-config.js';
+import Site from './schemas/Site.js';
+import User from './schemas/User.js';
+import { logger } from './logger-backend.js';
+import cors from 'cors';
+import Queue from 'bull';
+import { WebSocketServer } from 'ws';
+import http from 'http';
+import Redis from 'ioredis';
 
 dotenv.config();
 
@@ -38,262 +38,262 @@ const nodeEnvironment = process.env.NODE_ENV;
 const pubClient = new Redis(redisUrl);
 const subClient = new Redis(redisUrl);
 const queueName =
-  nodeEnvironment === "production" ? "prodUserQueue" : "devUserQueue";
+	nodeEnvironment === 'production' ? 'prodUserQueue' : 'devUserQueue';
 const userQueue = new Queue(queueName, redisUrl);
 
 // Connect to mongodb
 connectToDb();
 
 wss.on('connection', (ws) => {
-  logger.info('New client connected.');
+	logger.info('New client connected.');
 
-  ws.on('message', (message) => {
-    const strMessage = message.toString();
-    if (strMessage === 'ping') {
-      logger.info('pong');
-      ws.send('pong');
-    }
-  });
+	ws.on('message', (message) => {
+		const strMessage = message.toString();
+		if (strMessage === 'ping') {
+			logger.info('pong');
+			ws.send('pong');
+		}
+	});
 
-  ws.on('close', () => {
-    logger.info('Client disconnected');
-    subClient.unsubscribe(channelName);
-  });
+	ws.on('close', () => {
+		logger.info('Client disconnected');
+		subClient.unsubscribe(channelName);
+	});
 
-  subClient.subscribe(channelName, (err) => {
-    if (err) {
-      logger.error(`Failed to subscribe: ${err}`);
-    } else {
-      logger.info('Subscription succeeded.');
-    }
-  });
+	subClient.subscribe(channelName, (err) => {
+		if (err) {
+			logger.error(`Failed to subscribe: ${err}`);
+		} else {
+			logger.info('Subscription succeeded.');
+		}
+	});
 
-  subClient.on('message', (channel, message) => {
-    if (channel === channelName) {
-      if (ws.readyState === ws.OPEN) {
-        ws.send(message);
-      } else {
-        logger.error('WebSocket is not open. Cannot send message.');
-      }
-    }
-  });
+	subClient.on('message', (channel, message) => {
+		if (channel === channelName) {
+			if (ws.readyState === ws.OPEN) {
+				ws.send(message);
+			} else {
+				logger.error('WebSocket is not open. Cannot send message.');
+			}
+		}
+	});
 });
 
 // Trust all proxies
-app.set("trust proxy", true);
+app.set('trust proxy', true);
 
 // Middleware
-app.use(express.static(path.join(dirName, "../public")));
+app.use(express.static(path.join(dirName, '../public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
-  session({
-    secret,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 24 * 60 * 60 * 1000,
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-    },
-    store: MongoStore.create({
-      client: mongoose.connection.getClient(),
-      collectionName: "sessions",
-      dbName: process.env.DB,
-    }),
-  }),
+	session({
+		secret,
+		resave: false,
+		saveUninitialized: false,
+		cookie: {
+			secure: process.env.NODE_ENV === 'production',
+			maxAge: 24 * 60 * 60 * 1000,
+			sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+		},
+		store: MongoStore.create({
+			client: mongoose.connection.getClient(),
+			collectionName: 'sessions',
+			dbName: process.env.DB,
+		}),
+	})
 );
 app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL_1, process.env.FRONTEND_URL_2],
-    credentials: true,
-  }),
+	cors({
+		origin: [process.env.FRONTEND_URL_1, process.env.FRONTEND_URL_2],
+		credentials: true,
+	})
 );
 app.use(passport.initialize());
 app.use(passport.session());
-app.use("/auth", authRoutes);
+app.use('/auth', authRoutes);
 app.use((err, req, res, next) => {
-  if (process.env.NODE_ENV === "production") {
-    logger.error(`Error in request ${req.method} ${req.originalUrl}:\n${err}`);
-    res.status(500).send("Something went wrong");
-  } else {
-    next(err);
-  }
+	if (process.env.NODE_ENV === 'production') {
+		logger.error(`Error in request ${req.method} ${req.originalUrl}:\n${err}`);
+		res.status(500).send('Something went wrong');
+	} else {
+		next(err);
+	}
 });
 
-app.post("/api/log", (req, res) => {
-  const { level, message } = req.body;
-  logger[level](message);
-  res.status(200).send("Log received");
+app.post('/api/log', (req, res) => {
+	const { level, message } = req.body;
+	logger[level](message);
+	res.status(200).send('Log received');
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(dirName, "../public", "index.html"));
+app.get('/', (req, res) => {
+	res.sendFile(path.join(dirName, '../public', 'index.html'));
 });
 
-app.get("/api/user", (req, res) => {
-  try {
-    // Check for an active session
-    if (!req.user) {
-      res.send(false);
-    } else {
-      res.send(req.user);
-    }
-  } catch (err) {
-    logger.error(`Error in request /api/user:\n${err}`);
-    res.status(500).json({ error: "Server error" });
-  }
+app.get('/api/user', (req, res) => {
+	try {
+		// Check for an active session
+		if (!req.user) {
+			res.send(false);
+		} else {
+			res.send(req.user);
+		}
+	} catch (err) {
+		logger.error(`Error in request /api/user:\n${err}`);
+		res.status(500).json({ error: 'Server error' });
+	}
 });
 
-app.get("/api/premade-configs", async (req, res) => {
-  try {
-    const arrConfigs = await getPremadeConfigs();
-    res.json(arrConfigs);
-  } catch (err) {
-    logger.error(`Error in request /api/premade-configs:\n${err}`);
-    res.status(500).json({ error: "Failed to fetch site configs." });
-  }
+app.get('/api/premade-configs', async (req, res) => {
+	try {
+		const arrConfigs = await getPremadeConfigs();
+		res.json(arrConfigs);
+	} catch (err) {
+		logger.error(`Error in request /api/premade-configs:\n${err}`);
+		res.status(500).json({ error: 'Failed to fetch site configs.' });
+	}
 });
 
-app.post("/api/user-configs", async (req, res) => {
-  try {
-    const { arrIds } = req.body;
-    const user = req.user;
+app.post('/api/user-configs', async (req, res) => {
+	try {
+		const { arrIds } = req.body;
+		const user = req.user;
 
-    if (!arrIds) {
-      const arrStoredIds = user._doc.sites;
-      const objStoredConfig = await getSelectedConfigs(arrStoredIds);
-      res.json(objStoredConfig);
-      return;
-    }
+		if (!arrIds) {
+			const arrStoredIds = user._doc.sites;
+			const objStoredConfig = await getSelectedConfigs(arrStoredIds);
+			res.json(objStoredConfig);
+			return;
+		}
 
-    const objConfig = await getSelectedConfigs(arrIds);
-    res.json(objConfig);
-  } catch (err) {
-    logger.error(`Error in request /api/user-configs:\n${err}`);
-    res.status(500).json({ error: "Failed to fetch site config." });
-  }
+		const objConfig = await getSelectedConfigs(arrIds);
+		res.json(objConfig);
+	} catch (err) {
+		logger.error(`Error in request /api/user-configs:\n${err}`);
+		res.status(500).json({ error: 'Failed to fetch site config.' });
+	}
 });
 
-app.post("/api/add-config", async (req, res) => {
-  try {
-    const user = req.user;
-    const { objSiteData } = req.body;
-    objSiteData.errorMessages = objSiteData.errorMessages.split(",");
-    const newSite = new Site(objSiteData);
-    await newSite.save();
-    user.sites.push(newSite._id);
-    await user.save();
+app.post('/api/add-config', async (req, res) => {
+	try {
+		const user = req.user;
+		const { objSiteData } = req.body;
+		objSiteData.errorMessages = objSiteData.errorMessages.split(',');
+		const newSite = new Site(objSiteData);
+		await newSite.save();
+		user.sites.push(newSite._id);
+		await user.save();
 
-    res.json({
-      id: newSite._id,
-    });
-  } catch (err) {
-    logger.error(`Error in request /api/add-config:\n${err}`);
-    res.status(500).json({ error: "Failed to insert site config." });
-  }
+		res.json({
+			id: newSite._id,
+		});
+	} catch (err) {
+		logger.error(`Error in request /api/add-config:\n${err}`);
+		res.status(500).json({ error: 'Failed to insert site config.' });
+	}
 });
 
-app.post("/api/remove-config", async (req, res) => {
-  const session = await mongoose.startSession();
-  try {
-    const user = req.user;
-    const { selectedValues } = req.body;
-    session.startTransaction();
+app.post('/api/remove-config', async (req, res) => {
+	const session = await mongoose.startSession();
+	try {
+		const user = req.user;
+		const { selectedValues } = req.body;
+		session.startTransaction();
 
-    for (const value of selectedValues) {
-      await Site.deleteOne({ _id: value }, { session });
-      await User.updateOne(
-        { _id: user._id },
-        { $pull: { sites: value } },
-        { session },
-      );
-    }
+		for (const value of selectedValues) {
+			await Site.deleteOne({ _id: value }, { session });
+			await User.updateOne(
+				{ _id: user._id },
+				{ $pull: { sites: value } },
+				{ session }
+			);
+		}
 
-    await session.commitTransaction();
+		await session.commitTransaction();
 
-    res.send();
-  } catch (err) {
-    await session.abortTransaction();
-    logger.error(`Error in request /api/remove-config:\n${err}`);
-    res.status(500).json({ error: "Failed to remove site config." });
-  } finally {
-    await session.endSession();
-  }
+		res.send();
+	} catch (err) {
+		await session.abortTransaction();
+		logger.error(`Error in request /api/remove-config:\n${err}`);
+		res.status(500).json({ error: 'Failed to remove site config.' });
+	} finally {
+		await session.endSession();
+	}
 });
 
-app.post("/api/listings", async (req, res) => {
-  try {
-    const { keywords, objConfig } = req.body;
-    const job = await userQueue.add(
-      { keywords, objConfig },
-      {
-        removeOnComplete: true,
-        removeOnFail: true,
-        attempts: 3,
-        timeout: 300000,
-      },
-    );
-    const jobId = job.id;
-    logger.info(`Job added to queue: ${job.id}`);
-    pubClient.publish(channelName, JSON.stringify({ jobId, status: "added" }));
-    res.json({ jobId });
-  } catch (err) {
-    logger.error(`Error in request /api/listings:\n${err}`);
-    res.status(500).json({ error: "Failed to add job to the queue." });
-  }
+app.post('/api/listings', async (req, res) => {
+	try {
+		const { keywords, objConfig } = req.body;
+		const job = await userQueue.add(
+			{ keywords, objConfig },
+			{
+				removeOnComplete: true,
+				removeOnFail: true,
+				attempts: 3,
+				timeout: 300000,
+			}
+		);
+		const jobId = job.id;
+		logger.info(`Job added to queue: ${job.id}`);
+		pubClient.publish(channelName, JSON.stringify({ jobId, status: 'added' }));
+		res.json({ jobId });
+	} catch (err) {
+		logger.error(`Error in request /api/listings:\n${err}`);
+		res.status(500).json({ error: 'Failed to add job to the queue.' });
+	}
 });
 
-app.post("/api/listings-mail", async (req, res) => {
-  const { html } = req.body;
-  logger.info(`\nHTML received:\n${html}`);
-  res.status(200).send("HTML received");
-  sendMail(html);
+app.post('/api/listings-mail', async (req, res) => {
+	const { html } = req.body;
+	logger.info(`\nHTML received:\n${html}`);
+	res.status(200).send('HTML received');
+	sendMail(html);
 });
 
-app.delete("/api/delete-user", async (req, res) => {
-  try {
-    const user = req.user;
-    if (!user) return res.status(404).json({ error: "User not found" });
+app.delete('/api/delete-user', async (req, res) => {
+	try {
+		const user = req.user;
+		if (!user) return res.status(404).json({ error: 'User not found' });
 
-    await deleteUser(user);
+		await deleteUser(user);
 
-    req.logout((err) => {
-      if (err) throw err;
-      req.session.destroy((sessionErr) => {
-        if (sessionErr) return next(sessionErr);
-        res.clearCookie("connect.sid");
-        res.status(200).json({ message: "User deleted and logged out" });
-      });
-    });
-  } catch (err) {
-    logger.error(`Error in request /api/delete-user:\n${err}`);
-    res.status(500).json({ error: "Error deleting user." });
-  }
+		req.logout((err) => {
+			if (err) throw err;
+			req.session.destroy((sessionErr) => {
+				if (sessionErr) return next(sessionErr);
+				res.clearCookie('connect.sid');
+				res.status(200).json({ message: 'User deleted and logged out' });
+			});
+		});
+	} catch (err) {
+		logger.error(`Error in request /api/delete-user:\n${err}`);
+		res.status(500).json({ error: 'Error deleting user.' });
+	}
 });
 
 server.listen(port, () => {
-  logger.info(`\nServer running at: http://localhost:${port}`);
+	logger.info(`\nServer running at: http://localhost:${port}`);
 });
 
-process.on("SIGINT", async () => {
-  try {
-    await mongoose.connection.close();
-    logger.info("\nMongoose disconnected through app termination");
-    process.exit(0);
-  } catch (err) {
-    logger.error(`\nError closing MongoDB connection: ${err}`);
-    process.exit(1);
-  }
+process.on('SIGINT', async () => {
+	try {
+		await mongoose.connection.close();
+		logger.info('\nMongoose disconnected through app termination');
+		process.exit(0);
+	} catch (err) {
+		logger.error(`\nError closing MongoDB connection: ${err}`);
+		process.exit(1);
+	}
 });
 
-process.on("SIGTERM", async () => {
-  try {
-    await mongoose.connection.close();
-    logger.info("\nMongoDB connection closed.");
-    process.exit(0);
-  } catch (err) {
-    logger.error(`\nError closing MongoDB connection: ${err}`);
-    process.exit(1);
-  }
+process.on('SIGTERM', async () => {
+	try {
+		await mongoose.connection.close();
+		logger.info('\nMongoDB connection closed.');
+		process.exit(0);
+	} catch (err) {
+		logger.error(`\nError closing MongoDB connection: ${err}`);
+		process.exit(1);
+	}
 });
