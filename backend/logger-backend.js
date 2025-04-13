8dc593ba92c1bd069e4a678d80e4f530756453d8
BrooksSuz <susorbrooks@gmail.com>
2025-04-13 12:34:16 -0400

    Fix logger

--- backend/logger-backend.js
+++ backend/logger-backend.js
@@ -1,29 +1,31 @@
-import dotenv from "dotenv";
-import pino from "pino";
+import dotenv from 'dotenv';
+import pino from 'pino';
 dotenv.config();
 
 const logger = pino({
-  level: process.env.NODE_ENV === "production" ? "warn" : "debug",
-  transport: process.env.NODE_ENV !== "production" && {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-      translateTime: "SYS:standard",
-      ignore: "pid,hostname",
-    },
-  },
+	level: process.env.NODE_ENV === 'production' ? 'warn' : 'debug',
+	transport: {
+		target:
+			process.env.NODE_ENV !== 'production' ? 'pino-pretty' : 'pino/transport',
+		options: {
+			colorize: true,
+			translateTime: 'SYS:standard',
+			ignore: 'pid,hostname',
+		},
+	},
 });
 
 const loggerFlexLogs = pino({
-  level: process.env.NODE_ENV === 'production' ? 'warn' : 'debug',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'SYS:standard',
-      ignore: 'pid,hostname',
-    },
-  },
+	level: process.env.NODE_ENV === 'production' ? 'warn' : 'debug',
+	transport: {
+		target:
+			process.env.NODE_ENV !== 'production' ? 'pino-pretty' : 'pino/transport',
+		options: {
+			colorize: true,
+			translateTime: 'SYS:standard',
+			ignore: 'pid,hostname',
+		},
+	},
 });
 
 export { logger, loggerFlexLogs };
