8c4403c563c0ad7db4210ab81f210cb781a56c24
BrooksSuz <susorbrooks@gmail.com>
2025-04-13 12:39:38 -0400

    Fix logger

--- backend/logger-backend.js
+++ backend/logger-backend.js
@@ -4,28 +4,32 @@ dotenv.config();
 
 const logger = pino({
 	level: process.env.NODE_ENV === 'production' ? 'warn' : 'debug',
-	transport: {
-		target:
-			process.env.NODE_ENV !== 'production' ? 'pino-pretty' : 'pino/transport',
-		options: {
-			colorize: true,
-			translateTime: 'SYS:standard',
-			ignore: 'pid,hostname',
-		},
-	},
+	transport:
+		process.env.NODE_ENV !== 'production'
+			? {
+					target: 'pino-pretty',
+					options: {
+						colorize: true,
+						translateTime: 'SYS:standard',
+						ignore: 'pid,hostname',
+					},
+			  }
+			: undefined,
 });
 
 const loggerFlexLogs = pino({
 	level: process.env.NODE_ENV === 'production' ? 'warn' : 'debug',
-	transport: {
-		target:
-			process.env.NODE_ENV !== 'production' ? 'pino-pretty' : 'pino/transport',
-		options: {
-			colorize: true,
-			translateTime: 'SYS:standard',
-			ignore: 'pid,hostname',
-		},
-	},
+	transport:
+		process.env.NODE_ENV !== 'production'
+			? {
+					target: 'pino-pretty',
+					options: {
+						colorize: true,
+						translateTime: 'SYS:standard',
+						ignore: 'pid,hostname',
+					},
+			  }
+			: undefined,
 });
 
 export { logger, loggerFlexLogs };
